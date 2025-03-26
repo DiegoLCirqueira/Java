0e06f5886a00771d4b281141e26364a5bf81d40c
Diego Lins Cirqueira <diegolinscirqueira@hotmail.com>
2025-03-26 19:30:01 -0300

    split

---
+++ aula059/Main1.java
@@ -0,0 +1,13 @@
+package aula059;
+
+public class Main1 {
+    public static void main(String[] args) {
+        String s = "potato apple lemon orange";
+
+        String[] vect = s.split(" ");
+        System.out.println(vect[0]);
+        System.out.println(vect[1]);
+        System.out.println(vect[2]);
+        System.out.println(vect[3]);
+    }
+}
